b0ffaee7f5683fcbc5e7ff71fa0fbde6c435a4ee
AleksandrL0579 <a.lelsan@ukr.net>
2023-04-27 18:01:00 +0300

    terminal

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,46 @@ const router = express.Router();
 const hbs = require("hbs");
 
 router.get("/", function (req, res, next) {
-  res.render("index");
-});
+  res.render("index", {
+ users: [
+      {
+        firstname: 'Taras',
+        lastname: null,
+        age: 25,
+        isAdmin: true,
+        coments: [
+          {
+            id: 4343,
+            text: 'Hello World',
+          },
+          {
+            id: 4444,
+            text: 'Hello Sasha',
+          },
+        ],
+      },
+      {
+        firstname: 'Dima',
+        lastname: 'Ivanov',
+        age: 27,
+        isAdmin: false,
+      },
+    ],
+  })
+})
 
 router.get("/1", function (req, res, next) {
   res.render("1-var", { 
-    name: "Dima"
-  });
-});
+    user: {
+      name: 'Taras',
+      lastname: 'Ivanov',
+      age: {
+        value: 10,
+      },
+    },
+    title: Math.random(),
+  })
+})
 
 router.get("/2", function (req, res, next) {
   res.render("2-with", { 
